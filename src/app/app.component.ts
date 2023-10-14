8f724e8b789038e00900d121e6ff807c8589c57a
Biswatosh01 <101652879+Biswatosh01@users.noreply.github.com>
2023-10-14 20:42:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  todoItems: any[] = [
+    {
+      name: 'Swimming',
+      completed: false,
+      subtasks: ['Put on swimsuit', 'Go to the pool', 'Swim for 30 minutes']
+    },
+    {
+      name: 'Breakfast',
+      completed: false,
+      subtasks: ['Prepare cereal', 'Eat breakfast','clean up']
+    },
+    {
+      name: 'College',
+      completed: false,
+      subtasks: ['Attend lectures', 'Study for exams']
+    },
+    {
+      name: 'Shopping',
+      completed: false,
+      subtasks: ['Make a shopping list', 'Go to the grocery store']
+    },
+    {
+      name: 'Dinner',
+      completed: false,
+      subtasks: ['Cook dinner', 'Eat dinner']
+    }
+  ];
+  todoText: string = ''; // Declare the todoText property
+  completedItems: any[] = [];
+
+  // Move completed tasks to the completedItems array
+  updateCompletedItems() {
+    this.completedItems = this.todoItems.filter(item => item.completed);
+  }
+  newSubtasks: string[] = [];
+  deleteTodo(index: number) {
+    this.todoItems.splice(index, 1);
+    // Optionally, update the completed items list
+    this.updateCompletedItems();
+  }
+  addTodo() {
+    if (this.todoText.trim() !== '') {
+      const newTodoItem = {
+        name: this.todoText,
+        completed: false,
+        subtasks: this.newSubtasks // Assign the new subtasks array
+      };
+
+      this.todoItems.push(newTodoItem);
+      this.todoText = '';
+      this.newSubtasks = []; // Clear the newSubtasks array
+
+      // Optionally, update the completed items list
+      this.updateCompletedItems();
+    }
+  }
+
+  // ...existing methods...
+
+  // Function to add a new subtask to the newSubtasks array
+  addSubtask() {
+    if (this.todoText.trim() !== '') {
+      this.newSubtasks.push(this.todoText);
+      this.todoText = ''; // Clear the input field
+    }
+  }
 }
